0a3e678afea9871d5e46b18d094a721d586820ab
rzoshin <raiyan435jgcc@gmail.com>
2026-03-27 15:21:28 +0600

    Initiated the project

---
+++ my-react-project/src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css'
+import Countries from './components/Countries/Countries'
+
+function App() {
+  
+
+  return (
+    <div>
+      <h1>React World on the go</h1>
+      <Countries>
+        
+      </Countries>
+    </div>
+  )
+}
+
+export default App
